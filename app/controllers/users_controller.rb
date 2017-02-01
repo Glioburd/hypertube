3a909fb053692a7347f56a0b798188c41c5661e6
Guilhem <glioburd@gmail.com>
2017-02-01 17:17:02 +0100

    page viewers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+	before_action :set_users, only: [:index]
+
+	def index
+		
+	end
+
+	private
+
+	def set_users
+		@users = User.all
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   get 'welcome/index'
   root to: "welcome#index"
+  get '/users', to: 'users#index', as: 'users'
+
 end
