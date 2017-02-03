cceeeb1be20efdbad97eed7bb8f7ab912540a928
Guilhem <glioburd@gmail.com>
2017-02-03 15:32:42 +0100

    pagination en cours

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,16 @@ class MoviesController < ApplicationController
 	def index
 		param = params[:term]
 		if !param
-			response = HTTParty.get('https://yts.ag/api/v2/list_movies.json?minimum_rating=6.2&sort_by=seeds&with_images=true&order_by=desc&limit=50&page=3')
+			response = HTTParty.get('https://yts.ag/api/v2/list_movies.json?minimum_rating=8.2&sort_by=seeds&with_images=true&order_by=desc&limit=50&page=1')
 			json = response.body
-			@results = Post.paginate :page => params[:page]
-			# @result = JSON.parse(json)
+			# @results = json.paginate :page => params[:page], :per_page => 20
+			@result = JSON.parse(json)
+			@result.each do |info|
+				if info[0] == 'movies'
+					puts info[1]
+				end
+			end
+			# @movies = @result['data']['movies']['title']
 		end
 		 # @movies = Post.paginate(:page => params[:page], :per_page => 20)
 	end
