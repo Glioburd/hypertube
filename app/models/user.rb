e4c8ceab2b6795fc2dd2b2057a78b9c18d7440ab
Herve LEBER <hleber@e2r10p2.42.fr>
2017-02-06 11:08:02 +0100

    update user without password onfirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:marvin, :facebook, :twitter]
   mount_uploader :avatar, AvatarUploader
   validates_uniqueness_of :login
+
+  def update_with_password(params, *options)
+    current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = if params[:password].blank? || valid_password?(current_password)
+      update_attributes(params, *options)
+    else
+      self.assign_attributes(params, *options)
+      self.valid?
+      self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      false
+    end
+
+    clean_up_passwords
+    result
+  end
   
   def self.new_with_session(params, session)
     super.tap do |user|
