ddda2fb83f648559fa8b0ca6736bb63e3fa9610e
Herve LEBER <hleber@e2r10p2.42.fr>
2017-02-06 16:13:17 +0100

    i18n acceuil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
 	before_action :set_locale
 
   	def set_locale
-  		I18n.locale = params[:locale] if params[:locale].present?
+  		I18n.locale = params[:locale] || I18n.default_locale
+  		Rails.application.routes.default_url_options[:locale]= I18n.locale
     	#I18n.locale = current_user.try(:locale) || I18n.default_locale
   	end
 
