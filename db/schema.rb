54d49fd88698e6338f41313358ff0bf286448bb3
Guilhem <glioburd@gmail.com>
2017-02-07 11:22:40 +0100

    coffee modif

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170206225109) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170206225109) do
     t.string   "avatar"
     t.string   "provider"
     t.string   "uid"
+    t.text     "avatar_url"
     t.text     "imageOauthUrl"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
